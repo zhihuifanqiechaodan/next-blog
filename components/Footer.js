3eff4646eec1f03002ff2aa9675187f031a97535
只会番茄炒蛋 <673395239@qq.com>
2020-08-29 08:01:27 +0800

    增加footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,17 +1,27 @@
+import { SafetyCertificateFilled, EditFilled, BellFilled } from '@ant-design/icons';
+
 import '../public/styles/components/footer.scss'
 
-const Footer = () => (
-    <div className="footer-container">
-        <div className="footer-comp-item">
-            <span>系统由 React+Node+Ant Desgin驱动 </span>
+const Footer = () => {
+    // 当前年份
+    const nowTime = new Date().getFullYear()
+
+    return (
+        <div className="footer-container">
+            <div className="footer-comp-item">
+                <BellFilled />
+                <span>系统由 Next + Egg + Ant Desgin 驱动</span>
+            </div>
+            <div className="footer-comp-item">
+                <SafetyCertificateFilled />
+                <a href="http://www.beian.miit.gov.cn/" target="_blank" rel="noopener noreferrer">京ICP备18034129号</a>
+            </div>
+            <div className="footer-comp-item">
+                <EditFilled />
+                <span>Gao Yu 2019 - {nowTime}</span>
+            </div>
         </div>
-        <div className="footer-comp-item">
-            <span>系统由 React+Node+Ant Desgin驱动 </span>
-        </div>
-        <div className="footer-comp-item">
-            <span>系统由 React+Node+Ant Desgin驱动 </span>
-        </div>
-    </div>
-)
+    )
+}
 
 export default Footer
